42c45cd5c07ea9045f759bb640131faa1cfd616d
gunvora <gunvor_a@telia.com>
2018-04-22 10:44:59 +0200

    First version with scss styring

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,52 @@
 import React from "react"
+import "./app.css"
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="App">
+        <div className="tabs-container">
+          <div className="tab-container-hidden">
+            <div className="xxx">Flyg</div>
+          </div>
+          <div className="tab-container">
+            <div>
+              <div className="tab-destination">
+                <div className="fly-from">
+                  <label>Flyg från:
+                    <input
+                      type="text"
+                      name="from"
+                      placeholder="Varifrån vill du åka?" />
+                  </label>
+                  <hr />
+                </div>
+                <div className="fly-from">
+                  <label>Flyg till:
+                    <input
+                      type="text"
+                      name="to"
+                      placeholder="Vart vill du åka?" />
+                  </label>
+                  <hr width="100%" />
+                </div>
+              </div>
+              <div className="suggested-destination">
+                <span>Stockholm, Göteborg</span> eller <span>Malmö</span>?
+              </div>
+              <div className="order-button_with_line">
+                <input
+                  className="order-button"
+                  type="submit"
+                  value="Sök och beställ" />
+                <div className="order-button-line">
+                  <hr width="100%" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
